ed38a1bb95291188a3b1ae5380f27a9dabbaf006
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2024-10-28 15:39:34 +0100

    esercizio completato

--- JS/json.js
+++ JS/json.js
@@ -1,1 +1,45 @@
-alert("ciao");
+// INPUT
+const userKm = 70;
+const userAge = 22;
+const prezzo = 0.21;
+
+const kilometri = parseInt(prompt('Quanti km vuoi percorrere?'));
+const eta = parseInt(prompt('Quanti anni hai?'));
+
+console.log(kilometri);
+console.log(eta);
+
+
+// ESECUZIONE LOGICA
+const prezzoIntero = prezzo * userKm;
+console.log(prezzoIntero);
+const decimali = prezzoIntero.toFixed(2);
+
+const prezzoScontato1 = ((prezzoIntero / 100) * 20);
+const prezzoFinale1 = prezzoIntero - prezzoScontato1;
+const prezzoScontato2 = ((prezzoIntero / 100) * 40);
+const prezzoFinale2 = prezzoIntero - prezzoScontato2;
+console.log(prezzoFinale1, prezzoFinale2);
+
+// OUTPUT
+let totale;
+
+if (eta < 18) {
+    totale = `Il prezzo del biglietto è ${prezzoFinale1}`;
+} else if (eta > 65) {
+    totale = `Il prezzo del biglietto è ${prezzoFinale2}`;
+} else {
+    totale = `Il prezzo del biglietto è ${prezzoIntero}`;
+}
+
+console.log(totale);
+
+
+
+
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,17 @@
+DATI
+Numero di km da percorrere: 70km
+Età del passeggero: 22
+Prezzo biglietto: 0.21€ al km
+Sconto: 20% per minorenni
+Sconto: 40% over 65
+
+ESECUZIONE LOGICA
+Chiedere all'utente il numero di km da percorrere 
+Chiedere all'utente la sua età
+Verificare il costo del suo tragitto in base ai km
+Verificare gli sconti ed esaminare se rientrano nella sua fascia d'età
+
+OUTPUT
+Se l'utente è minorenne mostra prezzo con 20%
+Se l'utente è over 65 mostra prezzo con 40%
+Altrimenti mostra prezzo intero
